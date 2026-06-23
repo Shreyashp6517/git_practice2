bbe16035e6769388cd555289d0ee443b759b2c24
Shreyash Patil <shreyash.patil@exazeit.com>
2026-06-23 12:10:04 +0530

    Use bcrypt for password hashing

--- auth.js
+++ auth.js
@@ -1,5 +1,10 @@
-// User authentication module
+// User authentication module (FEATURE VERSION)
 function login(username, password) {
-console.log(`Logging in $(username)`);
-return true;
+  console.log(`Logging in ${username} - FEATURE`);
+  // DIFFERENT: Use bcrypt instead
+  const bcrypt = require('bcrypt');
+  bcrypt.hash(password, 10, (err, hash) => {
+    console.log('Password hashed');
+  });
+  return true;
 }
